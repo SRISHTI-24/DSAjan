b9d5ca0428acff73fe5ec1bc89c2f454c065053d
SRISHTI-24 <srishtimsaxena2002@gmail.com>
2022-01-17 14:41:43 +0530

    without using array

---
+++ src/DE2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DE2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n =s.nextInt();
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+            f=f*i;
+        }
+        System.out.println(f);
+        int c=0;
+        while(f!=0)
+        {
+            int a = f%10;
+            if(a==0)
+            {
+                c++;
+            }
+            else
+            {
+                break;
+            }
+            f=f/10;
+        }
+        System.out.println(c);
+
+    }
+}
